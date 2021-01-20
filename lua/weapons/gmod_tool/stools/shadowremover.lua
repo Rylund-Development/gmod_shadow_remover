2459b03bb911aeaa36633b4a4382782aac8f0d3b
Martin Rylund <martin@rylund.eu>
2021-01-21 00:59:41 +0100

    Cleaned up code and updated controls
    + Changed hold key for making props darker and brighter to IN_USE
    + Changed brightness value range from 0.1-0.99 to 0-1 due to better color saving
    + Changed comment style and updated copyright

--- lua/weapons/gmod_tool/stools/shadowremover.lua
+++ lua/weapons/gmod_tool/stools/shadowremover.lua
@@ -1,106 +1,92 @@
+-----------------------------------------------------------------------------
+--
+--      © 2020 Rylund Development (info@rylund.dev) - All Rights Reserved
+--
+-----------------------------------------------------------------------------
 
---[[-----------------------------------------------------------------------------------
+TOOL.Category = "Render"
+TOOL.Author = "SnowredWolf"
+TOOL.Name = "#tool.shadowremover.name"
+TOOL.Desc = "#tool.shadowremover.desc"
+TOOL.ConfigName = ""
+TOOL.ClientConVar[ "valueInterval" ] = 10
 
-		Tool Creator - SnowredWolf | STEAM_0:0:41063225 | https://SnowredWolf.net
-		Tool Purpose - Remove shadows from props
+shadowremovertool = {}
+shadowremovertool.proplist = shadowremovertool.proplist or {}
 
--------------------------------------------------------------------------------------]]
-
-
---[[-------------------------------------------------------------------------
-		Default tool stuff
----------------------------------------------------------------------------]]
-	TOOL.Category = "Render"
-	TOOL.Author = "SnowredWolf"
-	TOOL.Name = "#tool.shadowremover.name"
-	TOOL.Desc = "#tool.shadowremover.desc"
-	TOOL.ConfigName = ""
-
-	TOOL.ClientConVar[ "valueInterval" ] = 10
-
---[[-------------------------------------------------------------------------
-		Variables and networking
----------------------------------------------------------------------------]]
-	shadowremovertool = {}
-	shadowremovertool.proplist = shadowremovertool.proplist or {}
-	if SERVER then
-		util.AddNetworkString("RemovePropShadow")
-		util.AddNetworkString("AddPropShadow")
-		util.AddNetworkString("RemoveShadowsWhenJoining")
-	end
+if SERVER then
+	util.AddNetworkString("RemovePropShadow")
+	util.AddNetworkString("AddPropShadow")
+	util.AddNetworkString("RemoveShadowsWhenJoining")
+end
 
 
---[[-------------------------------------------------------------------------
-		Setting up the tool options
----------------------------------------------------------------------------]]
-	if CLIENT then
-		TOOL.Information = {
-			{ name = "info", stage = 1 },
-			{ name = "left" },
-			{ name = "right" },
-			{ name = "reload"},
-			{ name = "reload_left", icon2 = "gui/lmb.png"},
-			{ name = "reload_right", icon2 = "gui/rmb.png"}
-		}
+if CLIENT then
+	TOOL.Information = {
+		{ name = "info", stage = 1 },
+		{ name = "left" },
+		{ name = "right" },
+		{ name = "reload"},
+		{ name = "left_use"},
+		{ name = "right_use"}
+	}
 
-		language.Add("tool.shadowremover.name", "Shadow Remover")
-		language.Add("tool.shadowremover.left", "Remove shadows")
-		language.Add("tool.shadowremover.right", "Add shadows")
-		language.Add("tool.shadowremover.reload", "Reset brightness")
-		language.Add("tool.shadowremover.reload_left", "Make the prop darker" )
-		language.Add("tool.shadowremover.reload_right", "Make the prop brighter" )
-		language.Add("tool.shadowremover.desc", "Used to disable map shadows from props, making you able to avoid pitch black props!")
-	end
+	language.Add("tool.shadowremover.name", "Shadow Remover")
+	language.Add("tool.shadowremover.left", "Remove shadows")
+	language.Add("tool.shadowremover.right", "Add shadows")
+	language.Add("tool.shadowremover.reload", "Reset brightness")
+	language.Add("tool.shadowremover.left_use", "Make the prop darker" )
+	language.Add("tool.shadowremover.right_use", "Make the prop brighter" )
+	language.Add("tool.shadowremover.desc", "Used to disable map shadows from props, making you able to avoid pitch black props!")
+end
 
---[[-------------------------------------------------------------------------
-		Handle what happens when using left click
----------------------------------------------------------------------------]]
+
+-----------------------------------------------------------------------------
+-- Handle what happens when using left click
+-----------------------------------------------------------------------------
 	function TOOL:LeftClick(trace)
 		local ent = trace.Entity
+		if not IsEntity(ent) or ent:GetClass() ~= "prop_physics" and ent:GetClass() ~= "prop_ragdoll" then return false end
+		if CLIENT then return true end
 
-		if IsEntity(ent) and (ent:GetClass() == "prop_physics" or ent:GetClass() == "prop_ragdoll") then
-			if CLIENT then return true end
-			if not self:GetOwner():KeyDown(IN_RELOAD) then
-				shadowremovertool.removepropshadow(ent)
-			else
-				shadowremovertool.makedarker(self, ent)
-			end
-
-			return true
+		if not self:GetOwner():KeyDown(IN_USE) then
+			shadowremovertool.removepropshadow(ent)
+		else
+			shadowremovertool.makedarker(self, ent)
 		end
-
-		return false
+		return true
 	end
 
---[[-------------------------------------------------------------------------
-		Handle what happens when using right click
----------------------------------------------------------------------------]]
+-----------------------------------------------------------------------------
+-- Handle what happens when using right click
+-----------------------------------------------------------------------------
 	function TOOL:RightClick(trace)
 		local ent = trace.Entity
-		if IsEntity(ent) and (ent:GetClass() == "prop_physics" or ent:GetClass() == "prop_ragdoll") then
-			if CLIENT then return true end
-			if not self:GetOwner():KeyDown(IN_RELOAD) then
-				shadowremovertool.addpropshadows(ent)
-			else
-				shadowremovertool.makebrighter(self, ent)
-			end
-			return true
+		if not IsEntity(ent) or ent:GetClass() ~= "prop_physics" and ent:GetClass() ~= "prop_ragdoll" then return false end
+		if CLIENT then return true end
+
+		if not self:GetOwner():KeyDown(IN_USE) then
+			shadowremovertool.addpropshadows(ent)
+		else
+			shadowremovertool.makebrighter(self, ent)
 		end
-
-		return false
+		return true
 	end
 
 
---[[-------------------------------------------------------------------------
-		Handle what happens when pressing reload
----------------------------------------------------------------------------]]
+-----------------------------------------------------------------------------
+-- Handle what happens when pressing reload
+-----------------------------------------------------------------------------
 	function TOOL:Reload(trace)
-		trace.ent:SetColor(ent.originalcolor)
+		local ent = trace.Entity
+		_, _, ent.v = ColorToHSV(ent.originalcolor)
+		ent:SetColor(ent.originalcolor)
+		return true
 	end
 
---[[-------------------------------------------------------------------------
-		Serverside functions to handle shadows
----------------------------------------------------------------------------]]
+-----------------------------------------------------------------------------
+-- Serverside functions to handle shadows
+-----------------------------------------------------------------------------
 	if SERVER then
 		local colnew
 		local h, s, v
@@ -130,22 +116,17 @@
 
 		function shadowremovertool.makedarker(self, ent)
 			if not shadowremovertool.proplist[ent] then return false end
-			if ent.v <= 0.1 then return end
 			h, s, _ = ColorToHSV(ent.originalcolor)
 			local newValue = ent.v - self:GetClientNumber('valueInterval', 10) / 100
-			ent.v = 0.1 < newValue and newValue or 0.1
+			ent.v = 0.0 <= newValue and newValue or 0.0
 			ent:SetColor(HSVToColor(h, s, ent.v))
 		end
 
 		function shadowremovertool.makebrighter(self, ent)
 			if not shadowremovertool.proplist[ent] then return false end
 			h, s, _ = ColorToHSV(ent.originalcolor)
-			if ent.v >= 0.99 then return end
-
 			local newValue = ent.v + self:GetClientNumber('valueInterval', 10) / 100
-
-			ent.v = 0.99 > newValue and newValue or 0.99
-
+			ent.v = 1.0 >= newValue and newValue or 1.0
 			ent:SetColor(HSVToColor(h, s, ent.v))
 		end
 
@@ -162,22 +143,18 @@
 		end)
 	end
 
---[[-------------------------------------------------------------------------
-		Build the tool panel
----------------------------------------------------------------------------]]
+-----------------------------------------------------------------------------
+-- Build the tool panel
+-----------------------------------------------------------------------------
 	function TOOL.BuildCPanel(panel)
 		panel:Help("Darkness interval in %")
 
-		panel:NumSlider("Percentage: ",
-			"shadowremover_valueInterval",
-			"1",
-			"100"
-		)
+		panel:NumSlider("Percentage: ", "shadowremover_valueInterval", "1", "100")
 	end
 
---[[-------------------------------------------------------------------------
-		Clientside networking
----------------------------------------------------------------------------]]
+-----------------------------------------------------------------------------
+-- Clientside networking
+-----------------------------------------------------------------------------
 	net.Receive("RemovePropShadow", function()
 		local ent = net.ReadEntity()
 		ent.RenderOverride = function(self) render.SuppressEngineLighting(true) self:DrawModel() render.SuppressEngineLighting(false) end
